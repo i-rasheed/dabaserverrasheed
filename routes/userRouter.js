d3da42814503a595ea8ed93e1ca41557994ab816
Rasheed <abdulrasheedozovehe@gmail.com>
2022-06-12 09:39:47 -0700

    changed message to msg in login

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
@@ -18,10 +18,6 @@ router.post("/register", async (req, res) => {
     //   return res
     //     .status(400)
     //     .json({ msg: "The password needs to be at least 5 characters long." });
-    // if (password !== passwordCheck)
-    //   return res
-    //     .status(400)
-    //     .json({ msg: "Enter the same password twice for verification." });
 
     const existingUser = await User.findOne({ email: email });
     if (existingUser)
@@ -51,9 +47,7 @@ router.post("/login", async (req, res) => {
 
     // validate
     if (!email || !password)
-      return res
-        .status(400)
-        .json({ message: "Not all fields have been entered." });
+      return res.status(400).json({ msg: "Not all fields have been entered." });
 
     const user = await User.findOne({ email: email });
     if (!user)
@@ -62,8 +56,7 @@ router.post("/login", async (req, res) => {
         .json({ msg: "No account with this email has been registered." });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ message: "Invalid credentials." });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     res.json({
@@ -78,39 +71,5 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
-
-// router.delete("/delete", auth, async (req, res) => {
-//   try {
-//     const deletedUser = await User.findByIdAndDelete(req.user);
-//     res.json(deletedUser);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-
-// router.post("/tokenIsValid", async (req, res) => {
-//   try {
-//     const token = req.header("x-auth-token");
-//     if (!token) return res.json(false);
-
-//     const verified = jwt.verify(token, process.env.JWT_SECRET);
-//     if (!verified) return res.json(false);
-
-//     const user = await User.findById(verified.id);
-//     if (!user) return res.json(false);
-
-//     return res.json(true);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-
-// router.get("/", auth, async (req, res) => {
-//   const user = await User.findById(req.user);
-//   res.json({
-//     displayName: user.displayName,
-//     id: user._id,
-//   });
-// });
 
 module.exports = router;
